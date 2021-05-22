4f7aee3c1c3d0252ccbd1799973879c29d64b822
Choejiyu <cjyu0802@gmail.com>
2021-05-22 20:00:27 +0900

    Kakao-pay-api

---
+++ kakaopay2105/src/main/java/com/example/kakaopay2105/web/dto/TestDTO.java
@@ -0,0 +1,11 @@
+package com.example.kakaopay2105.web.dto;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+@Getter
+@RequiredArgsConstructor
+public class TestDTO {
+    private final String name;
+    private final int amount;
+}
